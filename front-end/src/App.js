185a94da375d0aee46e49d2cafeacb741651daf9
Rajesh Pawar <slayerklaus07@gmail.com>
2023-05-07 20:50:46 +0530

    Create front-end
    Added REact with basic routing using React Router and some CSS.

---
+++ front-end/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// In this Project we will be using React-router-dom for Routing
+// To use react-router-dom, we have to encase its components(Link,Routes...) in
+//<BrowserRouter></BrowserRouter>
+
+//using React-router looks easy compared to going thru with fetch api method
+// where explicit dev is more expected
+
+//LINK - is the element which is used to navigate through routes... just like <a></a>
+//ROUTE - defines what needs to be rendered When the UI url matches its path.
+
+// i m guessing, we can pass components to ROUTE.element
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<Header />}></Route>
+          <Route path="/add" element={<h1>Add Product component</h1>}></Route>
+          <Route
+            path="/update"
+            element={<h1>Update Product component</h1>}></Route>
+          <Route path="/logout" element={<h1>Logout component</h1>}></Route>
+          <Route path="/profile" element={<h1>Profile component</h1>}></Route>
+        </Routes>
+
+        <h1>E_Dashboard</h1>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//In this project we are using react-router-dom
+// basic css added to ul using the className attribute
+
+// we use <Routes><Route></Route></Routes> tag from react-router-dom to assign routes to links
+// Routes is done in App.js
+
+const Nav = () => {
+  return (
+    <>
+      <ul className="nav-ul">
+        <li>
+          <Link to={"/"}>Products</Link>
+        </li>
+        <li>
+          <Link to={"/add"}>Add Product</Link>
+        </li>
+        <li>
+          <Link to={"/update"}>Update Product</Link>
+        </li>
+        <li>
+          <Link to={"/logout"}>Logout</Link>
+        </li>
+        <li>
+          <Link to={"/profile"}>Profile</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+export default Nav;
